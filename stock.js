666be4a6fe6155fe30dd5790375ae108706df804
mistiplay <rrrichard.g92@gmail.com>
2025-12-26 23:11:56 -0500

    Add files via upload

---
+++ stock.js
@@ -0,0 +1,24 @@
+// PANEL DE CONTROL DE DISPONIBILIDAD
+// Cambia 'disponible' por 'agotado' según necesites.
+// Asegúrate de escribirlo exactamente igual (minúsculas).
+
+const stockData = {
+    'Netflix': 'agotado',
+    'Disney+': 'disponible',
+    'HBO Max': 'disponible',
+    'Paramount+': 'disponible',
+    'Prime Video': 'disponible',
+    'Vix Premium': 'disponible',
+    'Crunchyroll': 'disponible',
+    'Apple TV+': 'disponible',
+    'Directv GO': 'disponible',
+    'Movistar TV': 'disponible',
+    'Tele Latino': 'disponible',
+    'Flujo TV': 'disponible',
+    'IPTV Player': 'disponible',
+    'Canva Pro': 'disponible',
+    'CapCut Pro': 'disponible',
+    'ChatGPT Plus': 'disponible',
+    'Spotify': 'disponible',
+    'Youtube Premium': 'disponible'
+};
